28e8e042b45576e6bbddceaaf9ef732085781a8d
Simith Bhojani <simith_b@yahoo.in>
2025-03-26 21:28:17 -0400

    optimized go code

--- go/main.go
+++ go/main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	_ "github.com/lib/pq" // PostgreSQL driver
+	// _ "github.com/lib/pq" // PostgreSQL driver
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 // Database connection string for Docker container to access PostgreSQL on the host machine
@@ -18,7 +20,13 @@ var db *sql.DB
 // Handler for the root route
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Querying the database to get all records from the users table
-	rows, err := db.Query("SELECT id, name, email FROM users") // Update with the correct columns
+	stmt, err := db.Prepare("SELECT id, name, email FROM users")
+	if err != nil {
+		http.Error(w, "Database query failed", http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close() 
+	rows, err := stmt.Query() // Update with the correct columns
 	if err != nil {
 		http.Error(w, "Database query failed", http.StatusInternalServerError)
 		return
@@ -44,14 +52,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handler_helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 // Main function
 func main() {
 	// Establishing a connection to the PostgreSQL database
 	var err error
-	db, err = sql.Open("postgres", connStr)
+	db, err = sql.Open("pgx", connStr)
 	if err != nil {
 		log.Fatal("Error opening database: ", err)
 	}
+
+	db.SetMaxOpenConns(50)       
+	db.SetMaxIdleConns(25)       
+	db.SetConnMaxLifetime(5 * time.Minute) 
+	
 	// Check if the database connection is available
 	if err := db.Ping(); err != nil {
 		log.Fatal("Error pinging database: ", err)
@@ -59,6 +76,7 @@ func main() {
 	fmt.Println("Connected to the PostgreSQL database successfully!")
 
 	// Handling HTTP requests
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler_helloWorld)
+	http.HandleFunc("/users", handler)
 	http.ListenAndServe(":8080", nil)
 }
